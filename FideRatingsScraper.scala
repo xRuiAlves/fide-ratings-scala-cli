24d10c2ede3f127238086c80926a21d35b6928b2
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-09-16 16:57:52 +0100

    Refactor as CLI tool

--- FideRatingsScraper.scala
+++ FideRatingsScraper.scala
@@ -1,34 +1,33 @@
 //> using scala "3.3.0"
 //> using jvm "temurin:17"
-//> using lib "com.typesafe.akka::akka-stream::2.8.4"
-//> using lib "com.typesafe.akka::akka-http::10.5.2"
 //> using lib "net.ruippeixotog::scala-scraper::3.1.0"
-
-import akka.actor.ActorSystem
-import akka.http.scaladsl._
-import akka.http.scaladsl.model._
-import scala.concurrent.Future
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
+//> using lib "io.circe::circe-generic::0.14.6"
 
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
-import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
-import net.ruippeixotog.scalascraper.model._
+import net.ruippeixotog.scalascraper.model.Document
+
+import io.circe.generic.auto.deriveEncoder
+import io.circe.syntax._
 
 object FideRatingsScraper {
-  final val FideId = "1962000"
-  final val FideRatingsUrl = s"https://ratings.fide.com/profile/$FideId/chart"
-
   def main(args: Array[String]): Unit = {
-    val page = getRatingsPage()
+    val fideId = args.headOption
+    if (fideId.isEmpty) {
+      error("Missing FIDE ID argument!")
+    }
+
+    val page = getRatingsPage(fideId.get)
     val ratings = getRatings(page)
 
-    println(s"Latest ratings: ${ratings.head}")
+    if (ratings.isEmpty) error(s"No information found for player with FIDE ID ${fideId.get}!")
+    else println(ratings.asJson)
   }
 
-  def getRatingsPage(): Document = JsoupBrowser().get(FideRatingsUrl)
+  def fideRatingsUrl(fideId: String): String = s"https://ratings.fide.com/profile/$fideId/chart"
+
+  def getRatingsPage(fideId: String): Document = JsoupBrowser().get(fideRatingsUrl(fideId: String))
 
   case class Ratings(
       standard: String,
@@ -36,11 +35,20 @@ object FideRatingsScraper {
       blitz: String,
       date: String
   )
+  object Ratings {
+    def empty = Ratings("", "", "", "")
+  }
 
   def getRatings(page: Document): List[Ratings] =
     (page >> elementList(".profile-table_chart-table tbody tr"))
       .map(tableRow => (tableRow >> texts("td")).toList)
-      .map { case List(date, standard, _, rapid, _, blitz, _) =>
-        Ratings(standard, rapid, blitz, date)
+      .map {
+        case List(date, standard, _, rapid, _, blitz, _) => Ratings(standard, rapid, blitz, date)
+        case _ => Ratings.empty
       }
+
+  def error(message: String) = {
+    System.err.println(message)
+    sys.exit(1)
+  }
 }
